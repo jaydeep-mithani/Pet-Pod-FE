e57552d979319d787ee4720c850e41a3f119e419
Jaydeep Mithani <jaydeepmithani13@gmail.com>
2025-08-11 01:01:01 +0530

    Policy modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,6 @@ export default function Home() {
         Pet Pod
       </h1>
       <h3 className="text-5xl animate-pulse">Coming Soon...</h3>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus corrupti
-        molestias ut id ipsum, quia alias molestiae doloribus, blanditiis
-        nesciunt, tenetur hic adipisci dicta labore qui optio officiis in
-        consectetur.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
-        veniam quia et error iusto recusandae ea doloribus rerum voluptatum
-        dignissimos pariatur illo exercitationem nulla quibusdam debitis
-        repudiandae optio molestias consequatur non aliquam? Quaerat debitis
-        dolores quidem voluptatibus quasi, provident nesciunt, qui nemo
-        voluptate deserunt repellendus molestias? Alias sapiente, sequi
-        similique, unde quae, odit minus assumenda expedita necessitatibus
-        possimus natus maxime! Ipsam omnis excepturi voluptates voluptatum,
-        laudantium illo velit saepe totam! Laboriosam, aliquid aperiam natus
-        labore odit quae et provident dicta quos similique ducimus tempore esse
-        nisi cupiditate rerum cumque mollitia eius laudantium exercitationem
-        saepe! Architecto praesentium repellat esse quia sit.
-      </p>
     </div>
   );
 }
